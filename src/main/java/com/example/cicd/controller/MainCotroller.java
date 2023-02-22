a06ff62392c703249f467f19975ac5c8c7383a73
kapsin2 <110449509+kapsin2@users.noreply.github.com>
2023-02-22 15:30:03 +0900

    ㅎㅎ2

--- src/main/java/com/example/cicd/controller/MainCotroller.java
+++ src/main/java/com/example/cicd/controller/MainCotroller.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainCotroller {
   @GetMapping("/hello")
   public String helloWorld() {
+    
     return "Hello World";
   }
 }
